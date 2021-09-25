6a3c910e772948168b9e0c473071466f183b9d9e
Benjamin Ung <Benjaminung@cpp.edu>
2021-09-24 17:12:25 -0700

    added some class work

---
+++ ClassExercises/add-wHeader.c
@@ -0,0 +1,6 @@
+#include "Header.h"  //will include all variables aand functions in header
+
+//Let's define the function taht was prototype in header.h
+int add(int a, int b){
+    return a + b;
+}
---
+++ ClassExercises/header.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_H // if multiple file have include header. it will only read once by compiler
+#define HEADER_H
+
+int add(int a, int b);  // an example function declaration
+
+#endif
